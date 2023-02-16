f8ce1d220b2a53381b4cd03fee249ba225ae7c37
Leonardo Brizolla <leonardobrizollars@gmail.com>
2023-02-15 22:10:22 -0300

    Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
 
 import "./global.css";
 
 export function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>Post</main>
+      </div>
+    </div>
+  );
 }
