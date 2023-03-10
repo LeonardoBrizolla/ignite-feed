9ddc255dc4fb3023054a2f402986ba3bf17c45b6
Leonardo Brizolla <leonardobrizollars@gmail.com>
2023-03-10 20:28:08 -0300

    Component: comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/leonardobrizolla.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Leonardo Brizolla</strong>
+              <time
+                title="22 de Fevereiro às 16:00"
+                dateTime="2023-02-22 16:00:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom dev!!, parabéns!! 👏🏼👏🏼</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
