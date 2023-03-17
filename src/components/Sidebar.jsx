5264e5b1af076fcd2f58d69e71b3ed374aeb5f74
Leonardo Brizolla <leonardobrizollars@gmail.com>
2023-03-17 01:30:48 -0300

    Componente: Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,8 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 import styles from "./Sidebar.module.css";
+
 
 export function Sidebar() {
   return (
@@ -11,10 +13,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/leonardobrizolla.png"
-        />
+        <Avatar src="https://github.com/leonardobrizolla.png" />
 
         <strong>Leonardo Brizolla</strong>
         <span>Web Developer</span>
